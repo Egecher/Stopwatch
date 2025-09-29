13a9cdffcd99d832fd2f6625dc28f113d1b17cd9
Egecher <141852296+Egecher@users.noreply.github.com>
2025-09-29 03:15:55 +0300

    refactor: splitsecond script edit

--- script.js
+++ script.js
@@ -4,19 +4,25 @@ const stop = document.getElementById("stop");
 const reset = document.getElementById("reset");
 
 let timer;
-let [hours, minutes, seconds] = [0, 0, 0];
+let [hours, minutes, seconds, splitSecond] = [0, 0, 0, 0];
 
 function updateDisplay() {
     let h = String(hours).padStart(2, '0');
     let m = String(minutes).padStart(2, '0');
     let s = String(seconds).padStart(2, '0');
-    stopwatch.textContent = `${h}:${m}:${s}`;
+    let ss = String(splitSecond).padStart(2, '0');
+    stopwatch.textContent = `${h}:${m}:${s}:${ss}`;
 }
 
 start.addEventListener('click', () => {
     if(!timer) {
         timer = setInterval(() => {
-            seconds++;
+            splitSecond++;
+            if(splitSecond === 100) {
+                splitSecond = 0;
+                seconds++;
+            }
+
             if(seconds === 60) {
                 seconds = 0;
                 minutes++
@@ -28,7 +34,7 @@ start.addEventListener('click', () => {
             }
 
             updateDisplay();
-        }, 1000);
+        }, 10);
     }
 });
 
@@ -40,6 +46,6 @@ stop.addEventListener('click', () => {
 reset.addEventListener('click', () => {
     clearInterval(timer);
     timer = null;
-    [hours, minutes, seconds] = [0, 0, 0];
+    [hours, minutes, seconds, splitSecond] = [0, 0, 0, 0];
     updateDisplay();
 });
